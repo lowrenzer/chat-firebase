741f353789ebae5dc05dd02366f557511f235e51
lowrenzer <rezapahlevi.25.rp@gmail.com>
2021-06-01 21:01:33 +0700

    first commit

---
+++ build/precache-manifest.239b656937f65edac231e2d0bfa25834.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "06fb94068a1e5e8794bd466f61d7278c",
+    "url": "/index.html"
+  },
+  {
+    "revision": "98178d58ac74484dec1d",
+    "url": "/static/css/main.9cb7495d.chunk.css"
+  },
+  {
+    "revision": "9f16669a78388b1c7e2d",
+    "url": "/static/js/2.479629ae.chunk.js"
+  },
+  {
+    "revision": "40f7fa7ab5996d1e53a916b11d456d6f",
+    "url": "/static/js/2.479629ae.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "98178d58ac74484dec1d",
+    "url": "/static/js/main.243714a4.chunk.js"
+  },
+  {
+    "revision": "9bc2349bfbe76e87395f",
+    "url": "/static/js/runtime-main.e5afff51.js"
+  }
+]);
